ae3cc7a4b1f44abc9dd3f8c36256b4f95fb02394
SQCodes <paulsqmagadi@gmail.com>
2025-05-20 23:59:29 +0300

    modified:   index.html 	modified:   scripts.js

--- scripts.js
+++ scripts.js
@@ -10,3 +10,17 @@ menuToggle.addEventListener('click', () => {
         menuToggle.innerHTML = '<i class="fa-solid fa-bars"></i>';
     }
 })
+
+const profileImageContainer = document.querySelector('.profile-image');
+let profileImages = document.querySelectorAll('.profile-image img');
+let slideIndex = 0;
+showSlides();
+function showSlides(){
+    for (image = 0; image < profileImages.length; image++) {
+    profileImages[image].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > profileImages.length) {slideIndex = 1}
+  profileImages[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 4000);
+}
